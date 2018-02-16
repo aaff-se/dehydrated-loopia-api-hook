ba3e91e385d2ab0182d72ac9a0c1d6fb956cd83a
Anton Andersson <anton@aaff.se>
2018-02-16 12:36:02 +0100

    initial

---
+++ loopia_hook.sh
@@ -0,0 +1,194 @@
+#!/usr/bin/env bash
+
+# This is built upon https://gist.github.com/jreinert/49aca3b5f3bf2c5d73d8 by Joakim Reinert, and edited to match the Loopia API
+#
+# Copyright (c) Anton Andersson. All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+API_URL='https://api.loopia.se/RPCSERV'
+TMPDIR='/tmp/loopia-acme'
+
+source "$BASEDIR/hooks/loopia/loopia-api-user.auth" # contains user and pass variables
+
+USER=$user
+PASS=$pass
+
+build_method_call() {
+	local method_name=$1
+	shift
+	echo -n "<?xml version=\"1.0\"?>"
+	echo -n "<methodCall>"
+	echo -n "<methodName>$method_name</methodName><params>"
+
+	for param in "$@"; do
+		echo -n "$param"
+	done
+	
+	echo -n "</params></methodCall>"
+}
+
+build_param() {
+	local value=$1
+	local type=${2:-string}
+	echo -n "<param><value><$type>"
+		echo -n "$value"
+	echo -n "</$type></value></param>"
+}
+
+build_array_update() {
+	local token=$1
+	local record_id=${2:-0}
+	echo -n '<param>'
+		echo -n '<value><struct>'
+			echo -n '<member>'
+				echo -n "<name>type</name>"
+				echo -n "<value><string>TXT</string></value>"
+			echo -n "</member>"
+			echo -n '<member>'
+				echo -n "<name>ttl</name>"
+				echo -n "<value><int>300</int></value>"
+			echo -n "</member>"
+			echo -n '<member>'
+				echo -n "<name>rdata</name>"
+				echo -n "<value><string>$token</string></value>"
+			echo -n "</member>"
+			echo -n '<member>'
+				echo -n "<name>record_id</name>"
+				echo -n "<value><int>$record_id</int></value>"
+			echo -n "</member>"
+			echo -n '<member>'
+				echo -n "<name>priority</name>"
+				echo -n "<value><int>1</int></value>"
+			echo -n "</member>"
+		echo -n "</struct></value>"
+	echo -n "</param>"
+}
+
+build_update_record_call() {
+	local subdomain=$1
+	local domain=$2
+	local token=$3
+	local record_id=${4:-0}
+	build_method_call 'updateZoneRecord' \
+		"$(build_param $USER)" \
+		"$(build_param $PASS)" \
+		"$(build_param $domain)" \
+		"$(build_param $subdomain)" \
+		"$(build_array_update $token $record_id)" 
+}
+
+build_get_records_call() {
+	local subdomain=$1
+	local domain=$2
+	build_method_call 'getZoneRecords' \
+		"$(build_param $USER)" \
+		"$(build_param $PASS)" \
+		"$(build_param $domain)" \
+		"$(build_param $subdomain)"
+}
+
+method_call() {
+	local call=$1
+	local result=$(curl -s -c "$TMPDIR/cookies" -d "$call" -H 'Content-Type: text/xml' "$API_URL")
+	echo "$result"
+	echo >&2
+	return 1
+}
+
+deploy_challenge() {
+	local domain=$1
+	local token=$2
+	local record_id=$3
+
+	local result=$(method_call \
+		"$(build_update_record_call "_acme-challenge" "$domain" "$token" "$record_id")")
+	
+	echo "$result"
+}
+
+clean_challenge() {
+	local domain=$1
+	local record_id=$2
+	local result=$(method_call \
+		"$(build_update_record_call "_acme-challenge" "$domain" "done" "$record_id")")
+}
+
+deploy_cert() {
+	
+	local DOMAIN=$1 
+	local KEYFILE=$2
+	local FULLCHAINFILE=$3
+	
+	ln -sf "$KEYFILE" "/etc/nginx/ssl/$DOMAIN/privkey.pem"
+	ln -sf "$FULLCHAINFILE" "/etc/nginx/ssl/$DOMAIN/fullchain.pem"
+	
+}
+
+get_txt_record() {
+	local domain=$1
+
+	local result=$(method_call \
+		"$(build_get_records_call "_acme-challenge" "$domain")")
+
+	local xpath='//methodResponse//params//member/name[text()="record_id"]/../value/int/text()'
+	echo "$result" | xmllint --xpath "$xpath" -
+}
+
+invalid_challenge() {
+	local RESPONSE=$2
+	echo "$RESPONSE"
+}
+
+unchanged_cert() {
+	local DOMAIN=$1 
+	local KEYFILE=$2
+	local FULLCHAINFILE=$3
+	
+	echo "$DOMAIN"
+	echo "$KEYFILE"
+	echo "$FULLCHAINFILE"
+}
+
+mkdir -p "$TMPDIR"
+
+case $1 in
+'deploy_challenge')
+	get_txt_record $2 > "$TMPDIR/$2.id" # get the record id from loopia for use later
+	deploy_challenge "$2" "$4" "$(cat "$TMPDIR/$2.id")"
+	;;
+'clean_challenge')
+# this seems to clear the challenge txt before letsencypt have had a chance to validate
+# so I moved the cleaning to when the cert is deployed instead
+
+#	clean_challenge "$2" "$(cat "$TMPDIR/$2.id")"
+	;;
+'deploy_cert')
+	deploy_cert "$2" "$3" "$5"
+	clean_challenge "$2" "$(cat "$TMPDIR/$2.id")"
+	# we don't need the record id any more
+	rm "$TMPDIR/$2.id"
+	# used to keep track of when a cert was last deployed
+	echo "deployed $2" > "$TMPDIR/$2.deploycert"
+	;;
+'unchanged_cert')
+	# used to keep track of when a cert was last requested - to see that cron works as it should
+	unchanged_cert "$2" "$3" "$5" > "$TMPDIR/$2.unchanged"
+	;;
+'invalid_challenge')
+	invalid_challenge "$2" > "$TMPDIR/$2.fail"
+	;;
+'request_failure')
+# do something
+	;;
+'generate_csr')
+# do something
+	;;
+'startup_hook')
+# do something
+	;;
+'exit_hook')
+# do something
+	;;
+esac
